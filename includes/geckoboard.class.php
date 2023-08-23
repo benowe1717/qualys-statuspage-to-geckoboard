f3a01bb147aff417341c7e98c3111c781638188d
Benjamin Owen <benjamin@projecttiy.com>
2023-08-23 14:17:12 -0400

    updated main.php to support pushing test data to geckoboard, added in a myGeckoboard class to handle pushing data to the configured geckoboard widget

---
+++ includes/geckoboard.class.php
@@ -0,0 +1,64 @@
+<?php
+
+    class myGeckoboard {
+
+        private $file = __DIR__ . "/.creds.ini";
+        private $apikey = "";
+        private $widget_key = "";
+
+        public $scheme = "https://";
+        public $domain = "";
+        public $version = "";
+        public $url = "";
+        public $status_codes = array(400 => "Response body is empty or invalid JSON", 401 => "You are not authorized to push to this widget",
+        403 => "Widget does not support push/Your API key is invalid", 404 => "The widget does not exist", 413 => "The request body is too large",
+        429 => "You have exceeded your rate limit");
+
+        function __construct() {
+            if(!is_file($this->file)) {
+                echo "ERROR: Unable to read credentials file!";
+                exit(1);
+            } else {
+                $arr = parse_ini_file($this->file, TRUE);
+            }
+
+            if(isset($arr["gecko"])) {
+                $this->apikey = $arr["gecko"]["apikey"];
+                $this->domain = $arr["gecko"]["domain"];
+                $this->version = $arr["gecko"]["version"];
+                $this->widget_key = $arr["gecko"]["widgetkey"];
+            } else {
+                echo "ERROR: Unable to get Geckoboard credentials!";
+                exit(1);
+            }
+
+            $this->url = "{$this->scheme}{$this->domain}/{$this->version}";
+        }
+
+        public function pushToGeckoboard(string $text) {
+            $url = "{$this->url}/send/{$this->widget_key}";
+            $payload["api_key"] = $this->apikey;
+            $payload["data"]["item"][0]["text"] = $text;
+            $payload["data"]["item"][0]["type"] = 1;
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            $response = curl_exec($ch);
+            $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            $msg = json_decode($response, TRUE);
+            if(isset($this->status_codes[$status_code])) {
+                return $arr = array("status" => $status_code, "msg" => "{$this->status_codes[$status_code]} :: {$msg['message']}");
+            } else {
+                return $arr = array("status" => $status_code, "msg" => $msg);
+            }
+        }
+
+    }
+
+?>
