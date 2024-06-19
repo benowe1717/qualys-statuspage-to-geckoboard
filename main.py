6f9dcd1cbb41573546098f25e4925e23639b2511
Benjamin Owen <benjamin@projecttiy.com>
2024-06-19 11:10:19 -0400

    geckoboard api functionality

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+from src.classes.geckoboard import GeckboardApi
 from src.classes.statuspage_api import StatusPageApi
 
 
 def main():
     credentials_file = './data/credentials.yaml'
     statuspage = StatusPageApi(credentials_file)
+    geckboard = GeckboardApi(credentials_file)
     result = statuspage.get_unresolved_incidents()
     if not result:
         exit(1)
